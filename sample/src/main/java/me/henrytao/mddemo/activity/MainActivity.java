204a0dc0eb69619868ca8ccabd2ff299b6328e0b
Henry Tao <hi@henrytao.me>
2015-09-05 07:17:26 +0700

    [DEVELOP] Polished sample theme

--- sample/src/main/java/me/henrytao/mddemo/activity/MainActivity.java
+++ sample/src/main/java/me/henrytao/mddemo/activity/MainActivity.java
@@ -2,13 +2,18 @@ package me.henrytao.mddemo.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import me.henrytao.mddemo.R;
 
 public class MainActivity extends AppCompatActivity {
+
+  @Bind(R.id.toolbar)
+  Toolbar vToolbar;
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
@@ -25,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    //ButterKnife.inject(this);
+    ButterKnife.bind(this);
+
+    setSupportActionBar(vToolbar);
   }
 
   //@OnClick(R.id.goto_button)
