12cb64e11e14bda69cb0fc9c3493a13a225d9240
Henry Tao <hi@henrytao.me>
2016-04-28 09:57:43 +0700

    Added left navigation drawer

--- sample/src/main/java/me/henrytao/mddemo/activity/MainActivity.java
+++ sample/src/main/java/me/henrytao/mddemo/activity/MainActivity.java
@@ -16,42 +16,68 @@
 
 package me.henrytao.mddemo.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import me.henrytao.mdcore.core.MdCore;
 import me.henrytao.mdcore.core.MdCompat;
+import me.henrytao.mdcore.core.MdCore;
 import me.henrytao.mddemo.R;
 
 /**
  * Created by henrytao on 4/26/16.
  */
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
   @Bind(R.id.drawer_layout)
   DrawerLayout vDrawerLayout;
 
+  @Bind(R.id.navigation_view_left)
+  NavigationView vNavigationViewLeft;
+
   @Bind(R.id.toolbar)
   Toolbar vToolbar;
+
+  private ActionBarDrawerToggle mActionBarDrawerToggle;
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+  }
+
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item) {
+    return false;
+  }
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     MdCore.init(this);
-    super.onCreate(savedInstanceState);
 
+    super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
-
-    setSupportActionBar(vToolbar);
 
     boolean isFitSystemWindows = ViewCompat.getFitsSystemWindows(vDrawerLayout);
     if (isFitSystemWindows) {
       MdCompat.enableTranslucentStatus(this);
     }
+
+    setSupportActionBar(vToolbar);
+
+    mActionBarDrawerToggle = new ActionBarDrawerToggle(this, vDrawerLayout, vToolbar, R.string.text_open, R.string.text_close);
+    vDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+    mActionBarDrawerToggle.syncState();
+
+    vNavigationViewLeft.setNavigationItemSelectedListener(this);
   }
 }
