defab26fe9cc1848974eec51bb869e44eb6288b3
Henry Tao <hi@henrytao.me>
2016-05-10 21:22:51 +0700

    Support typographic

--- mdcore/src/main/java/me/henrytao/mdcore/core/MdLayoutInflaterFactory.java
+++ mdcore/src/main/java/me/henrytao/mdcore/core/MdLayoutInflaterFactory.java
@@ -19,15 +19,21 @@ package me.henrytao.mdcore.core;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v4.view.LayoutInflaterFactory;
 import android.support.v7.app.AppCompatDelegate;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import me.henrytao.mdcore.R;
 import me.henrytao.mdcore.utils.Ln;
@@ -38,7 +44,21 @@ import me.henrytao.mdcore.widgets.internal.MdCheckBox;
  */
 public class MdLayoutInflaterFactory implements LayoutInflaterFactory {
 
+  private static final int FONT_WEIGHT_BLACK = 60;
+
+  private static final int FONT_WEIGHT_BOLD = 50;
+
+  private static final int FONT_WEIGHT_LIGHT = 20;
+
+  private static final int FONT_WEIGHT_MEDIUM = 40;
+
+  private static final int FONT_WEIGHT_REGULAR = 30;
+
+  private static final int FONT_WEIGHT_THIN = 10;
+
   private static final String SUPPORT_CHECK_BOX = "CheckBox";
+
+  private static final Map<String, Typeface> sFontCaches = new HashMap<>();
 
   private final AppCompatDelegate mDelegate;
 
@@ -62,10 +82,15 @@ public class MdLayoutInflaterFactory implements LayoutInflaterFactory {
 
     if (view instanceof ImageView) {
       supportImageView(context, (ImageView) view, attrs);
-    } else if (view instanceof CheckBox) {
+    }
+    if (view instanceof CheckBox) {
       supportCheckBox(context, (CheckBox) view, attrs);
-    } else if (view instanceof Button) {
+    }
+    if (view instanceof Button) {
       supportButton(context, (Button) view, attrs);
+    }
+    if (view instanceof TextView) {
+      supportTextView(context, (TextView) view, attrs);
     }
     return view;
   }
@@ -113,5 +138,57 @@ public class MdLayoutInflaterFactory implements LayoutInflaterFactory {
     }
     a.recycle();
     view.setEnabled(isEnabled);
+  }
+
+  private void supportTextView(Context context, TextView view, AttributeSet attrs) {
+    String typography = null;
+    int fontWeight = 0;
+
+    TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.MdTypography, 0, 0);
+    try {
+      fontWeight = a.getInt(R.styleable.MdTypography_mdFontWeight, 0);
+      typography = a.getString(R.styleable.MdTypography_mdTypography);
+    } catch (Exception ignore) {
+    }
+    a.recycle();
+
+    Typeface typeface = null;
+    if (!TextUtils.isEmpty(typography)) {
+      if (!sFontCaches.containsKey(typography)) {
+        sFontCaches.put(typography, Typeface.createFromAsset(context.getAssets(), typography));
+      }
+      typeface = sFontCaches.get(typography);
+    } else if (fontWeight > 0) {
+      typography = null;
+      switch (fontWeight) {
+        case FONT_WEIGHT_THIN:
+          typography = MdCompat.getStringFromAttribute(context, R.attr.mdTypography_thin);
+          break;
+        case FONT_WEIGHT_LIGHT:
+          typography = MdCompat.getStringFromAttribute(context, R.attr.mdTypography_light);
+          break;
+        case FONT_WEIGHT_REGULAR:
+          typography = MdCompat.getStringFromAttribute(context, R.attr.mdTypography_regular);
+          break;
+        case FONT_WEIGHT_MEDIUM:
+          typography = MdCompat.getStringFromAttribute(context, R.attr.mdTypography_medium);
+          break;
+        case FONT_WEIGHT_BOLD:
+          typography = MdCompat.getStringFromAttribute(context, R.attr.mdTypography_bold);
+          break;
+        case FONT_WEIGHT_BLACK:
+          typography = MdCompat.getStringFromAttribute(context, R.attr.mdTypography_black);
+          break;
+      }
+      if (!TextUtils.isEmpty(typography)) {
+        if (!sFontCaches.containsKey(typography)) {
+          sFontCaches.put(typography, Typeface.createFromAsset(context.getAssets(), typography));
+        }
+        typeface = sFontCaches.get(typography);
+      }
+    }
+    if (typeface != null) {
+      view.setTypeface(typeface);
+    }
   }
 }
